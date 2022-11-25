df7b194848d72e9dd70f211b06a2e6af5103b14f
davshibru <davshibru@gmail.com>
2022-11-25 13:17:58 +0600

    replace box to dialog for ErrorDialogFragmentScreen function set width connected dismiss function

--- app/src/main/java/com/advmeds/cliniccheckinapp/dialog/screen/DialogScreen.kt
+++ app/src/main/java/com/advmeds/cliniccheckinapp/dialog/screen/DialogScreen.kt
@@ -10,7 +10,10 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -18,6 +21,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.advmeds.cliniccheckinapp.R
 
 @Composable
@@ -28,7 +33,7 @@ fun CheckingDialogFragmentScreen() {
     ) {
         Card(
             colors = CardDefaults.cardColors(
-                containerColor =  MaterialTheme.colorScheme.surface,
+                containerColor = MaterialTheme.colorScheme.surface,
             ),
             shape = RoundedCornerShape(10.dp)
         ) {
@@ -51,48 +56,61 @@ fun CheckingDialogFragmentScreen() {
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ErrorDialogFragmentScreen(message: String, closeDialog: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(start = 146.dp, end = 146.dp)
-            .clickable(onClick = closeDialog),
-        contentAlignment = Alignment.Center
-    ) {
-        Card(
-            colors = CardDefaults.cardColors(
-                containerColor =  MaterialTheme.colorScheme.surface,
-            ),
-            shape = RoundedCornerShape(10.dp)
-        ) {
-            Row(
-                modifier = Modifier
-                    .padding(20.dp), verticalAlignment = Alignment.CenterVertically
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_baseline_error),
-                    contentDescription = "error"
-                )
-                Column(modifier = Modifier.padding(start = 8.dp)) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = stringResource(id = R.string.fail_to_check),
-                        color = colorResource(id = R.color.error),
-                        fontSize = 48.sp
-                    )
 
-                    Text(
+    val openDialog = remember { mutableStateOf(true) }
+
+    if (openDialog.value) {
+
+        Dialog(onDismissRequest = {
+            openDialog.value = false
+            closeDialog()
+        },
+            properties = DialogProperties(
+                usePlatformDefaultWidth = false,
+                dismissOnBackPress = false,
+            ),
+            content = {
+                Card(
+                    modifier = Modifier
+                        .padding(start = 146.dp, end = 146.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.surface,
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                {
+                    Row(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 4.dp),
-                        text = if (message == null || message == "") "尚未到 下午 報到時間，請於 13:00~17:00 前往報到 或洽服務人員" else message,
-                        color = Color.Black,
-                        fontSize = 36.sp
-                    )
+                            .padding(20.dp), verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_baseline_error),
+                            contentDescription = "error"
+                        )
+                        Column(modifier = Modifier.padding(start = 8.dp)) {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = stringResource(id = R.string.fail_to_check),
+                                color = colorResource(id = R.color.error),
+                                fontSize = 48.sp
+                            )
+
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 4.dp),
+                                text = if (message == null || message == "") "尚未到 下午 報到時間，請於 13:00~17:00 前往報到 或洽服務人員" else message,
+                                color = Color.Black,
+                                fontSize = 36.sp
+                            )
+                        }
+                    }
                 }
             }
-        }
+        )
     }
 }
 
@@ -107,7 +125,7 @@ fun SuccessDialogFragmentScreen(closeDialog: () -> Unit) {
     ) {
         Card(
             colors = CardDefaults.cardColors(
-                containerColor =  MaterialTheme.colorScheme.surface,
+                containerColor = MaterialTheme.colorScheme.surface,
             ),
             shape = RoundedCornerShape(10.dp)
         ) {
