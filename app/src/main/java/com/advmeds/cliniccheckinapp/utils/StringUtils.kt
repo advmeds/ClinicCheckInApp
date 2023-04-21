f8a90c4425190402870444209f6dcd57ad5735d7
davshibru <davshibru@gmail.com>
2023-04-21 11:48:39 +0600

    remake clinic id dialog

--- app/src/main/java/com/advmeds/cliniccheckinapp/utils/StringUtils.kt
+++ app/src/main/java/com/advmeds/cliniccheckinapp/utils/StringUtils.kt
@@ -1,5 +1,6 @@
 package com.advmeds.cliniccheckinapp.utils
 
+import android.text.Editable
 import java.util.regex.Pattern
 
 /** 是否為台灣身分證或是居留證 */
@@ -13,3 +14,5 @@ val String.isNationId: Boolean
 /** 是否為屏基病例號 */
 val String.isPTCHCaseId: Boolean
     get() = true
+
+fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
