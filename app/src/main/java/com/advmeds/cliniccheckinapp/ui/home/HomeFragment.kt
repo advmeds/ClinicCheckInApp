cc8af95bbe1cf0ad97be021565a2ff9e0e59de63
davshibru <davshibru@gmail.com>
2023-04-26 13:31:06 +0600

    add dialog ui: change dept id , queueing board setting dialog

--- app/src/main/java/com/advmeds/cliniccheckinapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/advmeds/cliniccheckinapp/ui/home/HomeFragment.kt
@@ -16,6 +16,7 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.ArrayAdapter
 import android.widget.RadioGroup
 import androidx.appcompat.app.AlertDialog
@@ -31,9 +32,11 @@ import com.advmeds.cliniccheckinapp.ui.MainActivity
 import com.advmeds.cliniccheckinapp.ui.inputPage.InputPageFragment
 import com.advmeds.cliniccheckinapp.utils.showOnly
 import kotlinx.android.synthetic.main.change_clinic_id_dialog.*
+import kotlinx.android.synthetic.main.change_dept_id_dialog.*
 import kotlinx.android.synthetic.main.change_doctor_id_dialog.*
 import kotlinx.android.synthetic.main.change_domain_dialog.*
 import kotlinx.android.synthetic.main.change_room_id_dialog.*
+import kotlinx.android.synthetic.main.queueing_board_setting_dialog.*
 import kotlinx.android.synthetic.main.settings_dialog.*
 import okhttp3.HttpUrl
 
@@ -113,7 +116,8 @@ class HomeFragment : Fragment() {
 
             val settingItems = requireContext().resources.getStringArray(R.array.setting_items)
 
-            val arrayAdapter = ArrayAdapter(requireContext(), R.layout.settings_list_item, settingItems)
+            val arrayAdapter =
+                ArrayAdapter(requireContext(), R.layout.settings_list_item, settingItems)
 
             dialog.setting_options_list_view.adapter = arrayAdapter
 
@@ -126,6 +130,8 @@ class HomeFragment : Fragment() {
                     1 -> onSetOrgIDItemClicked()
                     2 -> onSetDoctorIDItemClicked()
                     3 -> onSetRoomIDItemClicked()
+                    4 -> onSetDeptIDItemClicked()
+                    5 -> onSetQueueingBoardSettingItemClicked()
                 }
             }
 
@@ -264,14 +270,6 @@ class HomeFragment : Fragment() {
         saveButton.setOnClickListener {
             val id = dialog.et_doctor_id_input.text.toString()
             dialog.dismiss()
-//            if (id.isNotBlank()) {
-//                viewModel.orgId = id
-//
-//                val intent = Intent(MainActivity.RELOAD_CLINIC_DATA_ACTION)
-//
-//                LocalBroadcastManager.getInstance(requireContext())
-//                    .sendBroadcast(intent)
-//            }
         }
 
         cancelButton.setOnClickListener {
@@ -301,14 +299,77 @@ class HomeFragment : Fragment() {
         saveButton.setOnClickListener {
             val id = dialog.et_room_id_input.text.toString()
             dialog.dismiss()
-//            if (id.isNotBlank()) {
-//                viewModel.orgId = id
-//
-//                val intent = Intent(MainActivity.RELOAD_CLINIC_DATA_ACTION)
-//
-//                LocalBroadcastManager.getInstance(requireContext())
-//                    .sendBroadcast(intent)
-//            }
+        }
+
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    private fun onSetDeptIDItemClicked() {
+
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.change_dept_id_dialog)
+
+        if (dialog.window == null)
+            return
+
+        dialog.window!!.setGravity(Gravity.CENTER)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        //dialog.et_clinic_id_input.setText(viewModel.orgId)
+
+        val saveButton = dialog.btn_change_dept_id_dialog_save
+        val cancelButton = dialog.btn_change_dept_id_dialog_cancel
+
+        saveButton.setOnClickListener {
+            val id = dialog.et_dept_id_input.text.toString()
+            dialog.dismiss()
+        }
+
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    private fun onSetQueueingBoardSettingItemClicked() {
+
+        val items = arrayListOf("Portrait", "Landscape")
+
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.queueing_board_setting_dialog)
+
+        if (dialog.window == null)
+            return
+
+        dialog.window!!.setGravity(Gravity.CENTER)
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val adapter =
+            ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, items)
+        dialog.queueing_board_setting_auto_complete_tv.setAdapter(adapter)
+
+
+        dialog.queueing_board_setting_switcher.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked)
+                dialog.queueing_board_setting_container.visibility = View.VISIBLE
+            else
+                dialog.queueing_board_setting_container.visibility = View.GONE
+        }
+
+        //dialog.et_clinic_id_input.setText(viewModel.orgId)
+
+        val saveButton = dialog.btn_qbs_dialog_save
+        val cancelButton = dialog.btn_qbs_dialog_cancel
+
+        saveButton.setOnClickListener {
+            val domain = dialog.et_qbs_irl_input.text.toString()
+            dialog.dismiss()
+
         }
 
         cancelButton.setOnClickListener {
